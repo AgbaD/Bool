edf2bef115aee36357715ff095c81abe56115475
Damilare Agba <damilareagba1@gmail.com>
2021-10-15 09:54:49 +0100

    starting over

--- learning/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Movies
-
-
-class MoviesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movies
-        fields = ['public_id', 'title', 'description', 'producer', 'rating']
--- learning/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('movies/', views.movies, name='movies')
-]
