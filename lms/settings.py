e56f4d8b30dca7ac10a15759916aa5b6d8b82a20
Damilare Agba <damilareagba1@gmail.com>
2021-10-17 17:01:58 +0100

    configuring project

--- lms/settings.py
+++ lms/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '780xm98h6yycxz%2tu00qmmn2+s6kgzj^ajcc$5ewj*(oyre3w'
+SECRET_KEY = os.getenv('SECRET_KEY') or '780xm98h6yycxz%2tu00qmmn2+s6kgzj^ajcc$5ewj*(oyre3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'tutor'
 ]
 
