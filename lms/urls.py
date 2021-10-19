d2637c9c26345d364177fa0a12cb8978447af4b0
Damilare Agba <damilareagba1@gmail.com>
2021-10-19 02:02:31 +0100

    adding student app

--- lms/urls.py
+++ lms/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tutor/', include('tutor.urls'))
+    path('tutor/', include('tutor.urls')),
+    path('student/', include('student.urls'))
 ]
