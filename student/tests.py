4287b079a2ad0e03581062349f453533d33b5743
Damilare Agba <damilareagba1@gmail.com>
2021-10-21 13:08:17 +0100

    testing|

--- student/tests.py
+++ student/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Student
 
-# Create your tests here.
+
+class StudentModelTest(TestCase):
+
+    def test_get_cart(self):
+        s = Student()
+        s = s.get_cart()
+        self.assertIsInstance(s, list)
+
+    def test_clear_cart(self):
+        s = Student()
+        s.clear_cart()
+        self.assertEqual(s.get_cart(), [])
+
+    def test_get_wishlist(self):
+        s = Student()
+        s = s.get_wishlist()
+        self.assertIsInstance(s, list)
+
+    def test_clear_wishlist(self):
+        s = Student()
+        s.clear_wishlist()
+        self.assertEqual(s.get_wishlist(), [])
--- tutor/tests.py
+++ tutor/tests.py
@@ -1,3 +1,40 @@
+from .models import CourseFiles
 from django.test import TestCase
+from .models import Tutor, Course
 
-# Create your tests here.
+
+class TutorModelTests(TestCase):
+
+    def test_get_enrolled_courses(self):
+        t = Tutor()
+        self.assertIsInstance(t.get_enrolled_courses(), dict)
+
+    def test_get_rating(self):
+        t = Tutor()
+        self.assertIsInstance(t.get_rating(), float)
+
+
+class CourseModelTests(TestCase):
+
+    def test_add_discount(self):
+        c = Course()
+        c.add_discount(20)
+        self.assertEqual(c.discount, True)
+
+    def test_remove_discount(self):
+        c = Course()
+        c.remove_discount()
+        self.assertEqual(c.discount, False)
+
+    def test_get_rating(self):
+        c = Course()
+        self.assertIsInstance(c.get_rating(), float)
+
+
+class CourseFileModelTest(TestCase):
+
+    def test_get_links(self):
+        c = CourseFiles()
+        c = c.get_links()
+        self.assertIsInstance(c, list)
+
